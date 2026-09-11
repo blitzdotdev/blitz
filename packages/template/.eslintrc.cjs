b0acb2a5a2ef5e87aa6fff9eda41b7703c65dfa0
pythonlearner1025 <mjsong2021@gmail.com>
2026-09-10 21:31:57 -0700

    refactor(kite3d): embed starter template

--- packages/template/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  env: {browser: true, es2022: true},
-  extends: ['eslint:recommended'],
-  parserOptions: {sourceType: 'module'},
-  ignorePatterns: ['.eslintrc.cjs'],
-}
