748e502a249094a90d87f60b3f5578d7ca21ec94
pythonlearner1025 <mjsong2021@gmail.com>
2026-09-14 12:06:15 -0700

    chore: strip editor manager state

--- src/paths.ts
+++ src/paths.ts
@@ -2,7 +2,6 @@ export const KITE3D_DIRECTORY = '.kite3d'
 export const DEPLOYS_PATH = `${KITE3D_DIRECTORY}/deploys.json`
 export const JOURNAL_PATH = `${KITE3D_DIRECTORY}/journal.jsonl`
 export const STATE_PATH = `${KITE3D_DIRECTORY}/state.json`
-export const CONSOLE_PATH = `${KITE3D_DIRECTORY}/console.log`
 export const THUMBS_DIRECTORY = `${KITE3D_DIRECTORY}/thumbs`
 export const BACKUPS_DIRECTORY = `${KITE3D_DIRECTORY}/backups`
 export const RUNNING_DIRECTORY = `${KITE3D_DIRECTORY}/running`
